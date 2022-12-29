493abc2e625fae9551cf19df9cd1e1daa996cfdb
Cianini Alessio <109857992+CianiniAlessio@users.noreply.github.com>
2022-12-29 08:28:07 +0100

    for iterating

--- OLS.py
+++ OLS.py
@@ -8,8 +8,7 @@ from statsmodels.api import OLS
 ###################### SOME CONSTANT ##########################
 ###############################################################
 
-ticker1 = "EURGBP=X"
-ticker2 = "GBPUSD=X"
+
 spread = 'SPREAD'
 moving_average = "MOVING AVERAGE"
 moving_average_std_dev = "MOVING AVERAGE STD"
@@ -29,71 +28,75 @@ TIME = 1000
 ##############################################################
 ##############################################################
 
-client = utils(ticker1,ticker2)
-df = pd.concat([pd.DataFrame(client.ticker1_close),pd.DataFrame(client.ticker2_close)], axis = 1)
-df.columns = [ticker1, ticker2]
+def doPairPnl(ticker1, ticker2):
+
+    client = utils(ticker1,ticker2)
+    df = pd.concat([pd.DataFrame(client.ticker1_close),pd.DataFrame(client.ticker2_close)], axis = 1)
+    df.columns = [ticker1, ticker2]
 
 
 
 
-# calculate spread
+    # calculate spread
 
 
-model = OLS(df[ticker1].iloc[:TIME],df[ticker2].iloc[:TIME]) #len(df[ticker2])
-model.fit()
-df[spread]  = df[ticker1] - model.normalized_cov_params[0] * df[ticker2]
-df[spread].iloc[:TIME].plot()
-#plt.ylabel("SPREAD")
-#plt.show()
-df[moving_average] = df[spread].rolling(30).mean()
-df[moving_average_std_dev] = df[spread].rolling(30).std()
-df[moving_average].iloc[:TIME].plot()
+    model = OLS(df[ticker1].iloc[:TIME],df[ticker2].iloc[:TIME]) #len(df[ticker2])
+    model.fit()
+    df[spread]  = df[ticker1] - model.normalized_cov_params[0] * df[ticker2]
+    df[spread].iloc[:TIME].plot()
+    #plt.ylabel("SPREAD")
+    #plt.show()
+    df[moving_average] = df[spread].rolling(30).mean()
+    df[moving_average_std_dev] = df[spread].rolling(30).std()
+    df[moving_average].iloc[:TIME].plot()
 
 
-#UPPER BAND AND LOWER BAND
+    #UPPER BAND AND LOWER BAND
 
 
-df[up_band] = df[moving_average] + 2*df[moving_average_std_dev]
-df[low_band] = df[moving_average] - 2*df[moving_average_std_dev]
-df[up_band].iloc[:TIME].plot()
-df[low_band].iloc[:TIME].plot()
-plt.show()
+    df[up_band] = df[moving_average] + 2*df[moving_average_std_dev]
+    df[low_band] = df[moving_average] - 2*df[moving_average_std_dev]
+    df[up_band].iloc[:TIME].plot()
+    df[low_band].iloc[:TIME].plot()
+    plt.title(ticker1 +  " " + ticker2)
+    plt.show()
 
 
-#LONG POS
+    #LONG POS
 
-df[long_entry] = df[spread] < df[low_band]
-df[long_exit] = df[spread] >= df[moving_average]
-
-
-df[LONG_POSITION] = np.nan
-df.loc[df[long_entry],LONG_POSITION] = 1
-df.loc[df[long_exit],LONG_POSITION] = 0
-
-df[LONG_POSITION] = df[LONG_POSITION].fillna(method='ffill')
-
-#SHORT POSITION
-
-df[short_entry] = df[spread] > df[up_band]
-df[short_exit] = df[spread] <= df[moving_average]
-
-df[SHORT_POSITION] = np.nan
-df.loc[df[long_entry],SHORT_POSITION] = -1
-df.loc[df[long_exit],SHORT_POSITION] = 0
-
-df[SHORT_POSITION] = df[SHORT_POSITION].fillna(method='ffill')
+    df[long_entry] = df[spread] < df[low_band]
+    df[long_exit] = df[spread] >= df[moving_average]
 
 
-df[POSITIONS] = df[LONG_POSITION] + df[SHORT_POSITION]
+    df[LONG_POSITION] = np.nan
+    df.loc[df[long_entry],LONG_POSITION] = 1
+    df.loc[df[long_exit],LONG_POSITION] = 0
 
-#PNL
+    df[LONG_POSITION] = df[LONG_POSITION].fillna(method='ffill')
 
-df[SPREAD_DIFFERENCE]= df[spread] - df[spread].shift(1)
-df[PNL] = df[POSITIONS].shift(1) + df[SPREAD_DIFFERENCE]
-df[CUM_PNL] = df[SPREAD_DIFFERENCE].cumsum()
+    #SHORT POSITION
 
-df[CUM_PNL].plot()
-plt.xlabel("Date")
-plt.ylabel(CUM_PNL)
-plt.legend()
-plt.show()
+    df[short_entry] = df[spread] > df[up_band]
+    df[short_exit] = df[spread] <= df[moving_average]
+
+    df[SHORT_POSITION] = np.nan
+    df.loc[df[long_entry],SHORT_POSITION] = -1
+    df.loc[df[long_exit],SHORT_POSITION] = 0
+
+    df[SHORT_POSITION] = df[SHORT_POSITION].fillna(method='ffill')
+
+
+    df[POSITIONS] = df[LONG_POSITION] + df[SHORT_POSITION]
+
+    #PNL
+
+    df[SPREAD_DIFFERENCE]= df[spread] - df[spread].shift(1)
+    df[PNL] = df[POSITIONS].shift(1) + df[SPREAD_DIFFERENCE]
+    df[CUM_PNL] = df[SPREAD_DIFFERENCE].cumsum()
+
+    df[CUM_PNL].plot()
+    plt.xlabel("Date")
+    plt.title(ticker1 + " " + ticker2)
+    plt.ylabel(CUM_PNL)
+    plt.legend()
+    plt.show()
